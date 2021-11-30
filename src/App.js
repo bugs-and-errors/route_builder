77ff0706d3d510ed7003f8da8f95c9170ff87933
monish karunakaran <ckmonish2000@gmail.com>
2021-11-30 14:53:55 +0530

    probably will work

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, { useState } from 'react';
 import ReactFlow, { removeElements, addEdge } from 'react-flow-renderer';
+import Modal from "antd"
 
-const initialElements = [
-  {
-    id: '1',
-    type: 'input',
-    data: { label: 'Input Node' },
-    position: { x: 0, y: 0 },
-  },
-  {
-    id: '2',
-    data: { label: 'Another Node' },
-    position: { x: 100, y: 125 },
-  },
-];
+// const initialElements = [
+//   {
+//     id: '1',
+//     type: 'input',
+//     data: { label: 'Input Node' },
+//     position: { x: 0, y: 0 },
+//   },
+//   {
+//     id: '2',
+//     data: { label: 'Another Node' },
+//     position: { x: 100, y: 125 },
+//   },
+//   {
+//     id: '4',
+//     type: 'output',
+//     data: {
+//       label: <div>
+//         <h1>one</h1>
+//         <button>xyz</button>
+//       </div>
+//     },
+//     position: { x: 100, y: 150 },
+//   }
+// ];
 
 const App = () => {
-  const [elements, setElements] = useState(initialElements);
+  const [elements, setElements] = useState([]);
+  const [openModal, setopenModal] = useState(false)
 
   const onElementsRemove = (elementsToRemove) =>
     setElements((els) => removeElements(elementsToRemove, els));
@@ -29,16 +42,9 @@ const App = () => {
 
   return (
     <div style={{ height: '100vh' }}>
-      <button onClick={() => {
-        const ele = [...elements]
-        ele.push({
-          id: '3',
-          type: 'output',
-          data: { label: '69' },
-          position: { x: 100, y: 150 },
-        })
-        setElements(ele)
-      }}>ADD BLOCK</button>
+
+      <button onClick={() => setopenModal(true)}>ADD BLOCK</button>
+
       <ReactFlow
         elements={elements}
         onElementsRemove={onElementsRemove}
