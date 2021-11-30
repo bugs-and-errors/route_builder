9614e9599b1862ea5bd68ddf3364d455870a357d
monish karunakaran <ckmonish2000@gmail.com>
2021-11-30 15:54:40 +0530

    push start block on the flow

--- src/Modal.js
+++ src/Modal.js
@@ -1,12 +1,27 @@
 import { useState } from 'react'
 
 
-export default function Modals({ toggleModal }) {
+export default function Modals({ toggleModal, push }) {
 
   const [Id, setId] = useState("")
   const [NodeType, setNodeType] = useState("")
 
-  // const handleOk = () => { }
+  const handleOk = () => {
+    if (NodeType === "start") {
+      push({
+        id: Id,
+        type: 'input',
+        data: {
+          label: <div>
+            {Id}
+            <button>x</button>
+          </div>
+        },
+        position: { x: 0, y: 0 },
+      })
+    }
+    handleCancel()
+  }
 
   const handleCancel = () => {
     toggleModal()
@@ -21,12 +36,12 @@ export default function Modals({ toggleModal }) {
         <input placeholder="Node ID/Name" className="modal_ip" value={Id} onChange={e => setId(e.target.value)} />
 
         <select placeholder="Node Type" className="modal_ip" value={NodeType} onChange={e => setNodeType(e.target.value)}>
-          <option>Start</option>
-          <option>Node</option>
-          <option>End</option>
+          <option value="start">Start</option>
+          <option value="node">Node</option>
+          <option value="end">End</option>
         </select>
 
-        <button className="btn" onClick={handleCancel} style={{ right: "60pt" }}>OK</button>
+        <button className="btn" onClick={handleOk} style={{ right: "60pt" }}>OK</button>
         <button className="btn" onClick={handleCancel}>Close</button>
       </div>
     </div>
