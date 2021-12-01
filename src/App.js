c9f47d85cdcccd1ca37c3a0573882b66d4db29f1
monish karunakaran <ckmonish2000@gmail.com>
2021-12-01 14:48:11 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -2,34 +2,6 @@ import React, { useState } from 'react';
 import ReactFlow, { removeElements, addEdge } from 'react-flow-renderer';
 import Modals from "./Modal"
 
-// const initialElements = [
-// {
-//   id: '1',
-//   type: 'input',
-//   data: { label: 'Input Node' },
-//   position: { x: 0, y: 0 },
-// },
-//   {
-//     id: '2',
-//     data: { label: 'Another Node' },
-//     position: { x: 100, y: 125 },
-//   },
-//   {
-//     id: '4',
-//     type: 'output',
-//     data: {
-//       label: <div>
-//         <h1>one</h1>
-//         <button>xyz</button>
-//       </div>
-//     },
-//     position: { x: 100, y: 150 },
-//   }
-// ];
-
-
-
-
 const App = () => {
   const [elements, setElements] = useState([]);
   const [openModal, setopenModal] = useState(false)
