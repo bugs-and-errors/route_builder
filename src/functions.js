bbfcd0c2bb526eefb680637023de07a859812658
monish karunakaran <ckmonish2000@gmail.com>
2021-12-07 18:02:17 +0530

    traverse

--- src/functions.js
+++ src/functions.js
@@ -3,22 +3,26 @@ const get_possible_endpoints = (joints, nodes, connect_val) => {
 
   const lst_of_pred = [connect_val]
 
-  let src = connect_val?.prev
   let target = connect_val?.succ
 
+  let src = connect_val?.prev
 
-  let get_pred_node = nodes?.filter(v => parseInt(v?.id) === parseInt(src))
+  for (let i = 0; i < nodes.length; i++) {
 
-  if (get_pred_node.length > 0 && get_pred_node[0]?.type !== "input") {
+    let get_pred_node = nodes?.filter(v => parseInt(v?.id) === parseInt(src))
 
-    get_pred_node = get_pred_node[0]?.id
+    if (get_pred_node.length > 0 && get_pred_node[0]?.type !== "input") {
 
-    let get_super_pred = joints?.filter(v => parseInt(v?.succ) === parseInt(get_pred_node))
+      get_pred_node = get_pred_node[0]?.id
 
-    lst_of_pred.push({ prev: get_super_pred[0]?.prev, succ: target })
+      let get_super_pred = joints?.filter(v => parseInt(v?.succ) === parseInt(get_pred_node))
 
+      lst_of_pred.push({ prev: get_super_pred[0]?.prev, succ: target })
+
+      src = get_super_pred[0]?.prev
+
+    }
   }
-
   console.log(lst_of_pred)
 }
 
