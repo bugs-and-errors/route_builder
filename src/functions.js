9edc65bdbba5f40bd68bbe98c4b4d3fcb4463f05
monish karunakaran <ckmonish2000@gmail.com>
2021-12-07 17:50:09 +0530

    pushing to the state

--- src/functions.js
+++ src/functions.js
@@ -1,10 +1,11 @@
 const get_possible_endpoints = (joints, nodes, connect_val) => {
-  console.log(joints, nodes, connect_val)
+  // console.log(joints, nodes, connect_val)
+
+  const lst_of_pred = [connect_val]
 
   let src = connect_val?.prev
   let target = connect_val?.succ
 
-  const lst_of_pred = [connect_val]
 
   let get_pred_node = nodes?.filter(v => parseInt(v?.id) === parseInt(src))
 
@@ -13,11 +14,12 @@ const get_possible_endpoints = (joints, nodes, connect_val) => {
     get_pred_node = get_pred_node[0]?.id
 
     let get_super_pred = joints?.filter(v => parseInt(v?.succ) === parseInt(get_pred_node))
-    console.log(get_super_pred)
+
+    lst_of_pred.push({ prev: get_super_pred[0]?.prev, succ: target })
 
   }
 
-  // lst_of_pred.push()
+  console.log(lst_of_pred)
 }
 
 
