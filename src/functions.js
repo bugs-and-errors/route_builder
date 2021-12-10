5ddfcc9deb19970304ac667922469176958278b0
monish karunakaran <ckmonish2000@gmail.com>
2021-12-11 00:13:50 +0530

    gods grace

--- src/functions.js
+++ src/functions.js
@@ -11,40 +11,18 @@ const get_possible_endpoints = (joints, nodes, connect_val) => {
   const lst_of_pred = [connect_val]
 
   let target = connect_val?.succ
-
   let src = connect_val?.prev
 
+  const queue = [connect_val]
 
+  while (queue.length > 0) {
 
-  let get_pred_node = get_pred_nodes(src, joints)
-  console.log(get_pred_node, "prev node =", src)
+    const curr_node = queue.shift()
+    let get_pred_node = get_pred_nodes(curr_node?.prev, joints)
+    queue.push(...get_pred_node)
+    lst_of_pred.push(...get_pred_node)
+  }
 
-  get_pred_node?.forEach(v => {
-    const paths_from_start = []
-    paths_from_start.push({ prev: v?.prev, succ: target })
-
-  })
-
-  // for (let i = 0; i < nodes.length; i++) {
-
-  // let get_pred_node = nodes?.filter(v => parseInt(v?.id) === parseInt(src))
-
-  // if (get_pred_node.length > 0 && get_pred_node[0]?.type !== "input") {
-
-  //   get_pred_node = get_pred_node[0]?.id
-
-  //   let get_super_pred = joints?.filter(v => parseInt(v?.succ) === parseInt(get_pred_node))
-
-  //   lst_of_pred.push({ prev: get_super_pred[0]?.prev, succ: target })
-
-  //   src = get_super_pred[0]?.prev
-
-  // }
-  // }
-  // console.log(lst_of_pred)
-
-
-  console.log(lst_of_pred, "bro")
   return lst_of_pred
 }
 
