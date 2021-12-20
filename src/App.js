691424a9664b3af8536d1e70d2a21434a9eeec4e
monish karunakaran <ckmonish2000@gmail.com>
2021-12-20 13:54:48 +0530

    for ward pass logic

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const App = () => {
   const [openModal, setopenModal] = useState(false)
   const [prev_succ, setprev_succ] = useState([])
 
-  const [skipState, setskipState] = useState([])
+  // const [skipState, setskipState] = useState([])
 
   const onElementsRemove = (elementsToRemove) =>
     setElements((els) => removeElements(elementsToRemove, els));
@@ -17,16 +17,16 @@ const App = () => {
 
   // console.log(prev_succ)
   const onConnect = (params) => {
-
+    console.log(params)
     // prev and successor track
     const x = [...prev_succ]
     x.push({ prev: params?.source, succ: params?.target })
     setprev_succ(x)
 
     // flow tracker
-    const possible_endpoints = get_possible_endpoints(prev_succ, elements, { prev: params?.source, succ: params?.target })
-    let skip = [...skipState, ...possible_endpoints]
-    setskipState(skip)
+    // const possible_endpoints = get_possible_endpoints(prev_succ, elements, { prev: params?.source, succ: params?.target })
+    // let skip = [...skipState, ...possible_endpoints]
+    // setskipState(skip)
 
     // flow chart track
     params = { ...params }
@@ -53,10 +53,10 @@ const App = () => {
     setprev_succ(x.filter(v => v?.succ !== id || v?.prev !== id))
   }
 
-  const skip_data = unique_vals(skipState)
+  // const skip_data = unique_vals(skipState)
 
   const CSV_Data = [["Predecessor", "Successor"], ...prev_succ?.map(v => { return [v?.prev, v?.succ] })]
-  const skip_CSV_Data = [["Predecessor", "Successor"], ...skip_data?.map(v => { return [v?.prev, v?.succ] })]
+  // const skip_CSV_Data = [["Predecessor", "Successor"], ...skip_data?.map(v => { return [v?.prev, v?.succ] })]
 
 
 
@@ -76,11 +76,36 @@ const App = () => {
               className="btn">Download Excel For No Skip</button>
           </CSVLink>}
 
-          {prev_succ?.length > 0 && <CSVLink data={skip_CSV_Data}>
+          <button onClick={() => {
+            // console.log(prev_succ, elements)
+            const start_nodes = []
+            elements?.forEach(v => { if (v?.type === "input") { start_nodes.push(v?.id) } })
+
+            const ele = {}
+
+            for (let x of start_nodes) {
+              const nodes = prev_succ?.filter(v => v?.prev === x)
+              const visited = [...nodes]
+              const queue = [...nodes]
+
+              while (queue.length > 0) {
+                const current_node = queue.shift()
+                const succesive_nodes = prev_succ?.filter(v => v?.prev === current_node?.succ)
+                // console.log(succesive_nodes)
+                queue.push(...succesive_nodes)
+                visited.push(...succesive_nodes)
+              }
+              ele[x] = visited
+            }
+            console.log(ele)
+          }}>Calculate Skip</button>
+
+
+          {/* {prev_succ?.length > 0 && <CSVLink data={skip_CSV_Data}>
             <button
               style={{ width: "fit-content", position: "relative", margin: "10pt", marginLeft: "0px" }}
               className="btn">Download Excel For Skip</button>
-          </CSVLink>}
+          </CSVLink>} */}
         </div>
 
       </div>
